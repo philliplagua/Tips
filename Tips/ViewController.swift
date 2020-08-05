f97233aa7f78f3a47772aaf0219e9d5f18d71dcf
Phillip Lagua <philliplagua@phillips-macbook-air.local>
2020-08-05 03:44:48 -0700

    Initial Commit

---
+++ Tips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tips
+//
+//  Created by Phillip Lagua on 8/5/20.
+//  Copyright © 2020 Phillip Lagua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
