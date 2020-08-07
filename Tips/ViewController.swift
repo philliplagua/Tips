bdb9eeb4a4e77c720e13d5b8f7b5ba381207aad3
Phillip Lagua <philliplagua@phillips-macbook-air.local>
2020-08-07 12:00:01 -0700

    Changes

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -6,15 +6,46 @@
 //  Copyright © 2020 Phillip Lagua. All rights reserved.
 //
 
-import UIKit
+import UIKit 
 
-class ViewController: UIViewController {
+class ViewController:
+    UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onTap(_ sender:
+        Any) {
+        print("Hello")
+        
+        view.endEditing(true)
+        
+    }
+    
+    @IBAction func calculateTips(_ sender: Any) {
+        
+        // Get bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        // Calculate the tip and total
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update the tip and total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
